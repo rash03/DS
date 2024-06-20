e4b088ddd1e43acab15d219bc6eaa55a44f2caf4
Rajat Shukla <rash1015@netcracker.com>
2024-06-20 14:21:33 +0530

    updated circular queue classes

---
+++ queue/src/main/java/ds/example/CircularQueue.java
@@ -0,0 +1,69 @@
+package ds.example;
+
+public class CircularQueue {
+
+    int[] arr;
+    int front;
+    int rear;
+    int size;
+
+    public CircularQueue(int size){
+        this.size = size;
+        this.arr = new int[size];
+        this.front = rear = -1;
+        System.out.println("Queue Created");
+    }
+
+    public boolean isEmpty(){
+        if(front == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isFull(){
+        if(rear == front+1){
+            return true;
+        }else if (front == 0 && rear+1 == size){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void enqueue(int value){
+        if(isFull()){
+            System.out.println("Queue is full");
+        }else if(isEmpty()){
+            front++;
+            rear++;
+            arr[rear] = value;
+            System.out.println("Successfully entered "+value+" in the queue");
+        }else{
+            if(rear+1 == size){
+                rear = 0;
+            }else{
+                rear++;
+            }
+            arr[rear] = value;
+            System.out.println("Successfully entered "+value+" in the queue");
+        }
+    }
+
+    public int dequeue(){
+
+        if(isEmpty()){
+            System.out.println("Queue is Empty");
+            return -1;
+        }else {
+            int element = arr[front];
+            arr[front] = -1;
+            front++;
+            if(front == size){
+                front = 0;
+            }
+            return element;
+        }
+    }
+}
--- queue/src/main/java/ds/example/QueueLA.java
+++ queue/src/main/java/ds/example/QueueLA.java
@@ -32,11 +32,44 @@ public class QueueLA {
     public void enqueue(int value){
         if (isFull()){
             System.out.println("Queue is full");
-        }else if(isEmpty()){
+        }else if(isEmpty()){ // if queue is empty then we have to set the beginning of queue.=> front=> from where the elements will be removed.
+            // for cases when queue is not empty then we only increment the rear => because elements are added in rear and removed from front
             front = 0;
             rear++;
             arr[rear] = value;
-            System.out.println();
+            System.out.println("Successfully entered "+value+" in the queue");
+        }else{
+            rear++;
+            arr[rear] = value;
+            System.out.println("Successfully entered "+value+" in the queue");
         }
+    }
+
+    public int dequeue(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }else{
+            int element = arr[front];
+            front++;
+            if(front > rear){
+                front = rear = -1;
+            }
+            return element;
+        }
+    }
+
+    public int peek(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }else{
+            return arr[front];
+        }
+    }
+
+    public void delete(){
+        arr = null;
+        System.out.println("Queue is successfully deleted");
     }
 }
